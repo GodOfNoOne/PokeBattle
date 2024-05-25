64dacdb1dbec1c4aea0513226b95358cc5532147
GodOfNoOne <115725718+GodOfNoOne@users.noreply.github.com>
2024-05-25 21:15:29 +0300

    Add files via upload

--- server_battle.py
+++ server_battle.py
@@ -5,225 +5,213 @@ from Pokemon import pokemon
 import random
 import time
 
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Game_Server:
+    def __init__(self, host=socket.gethostname(), port=6969):
+        self.host = host
+        self.port = port
+        self.server_socket = None
+        self.main_server_socket = None
+        self.clients = []
+        self.pokemon_teams = []
+    def start_server(self):
+        # Create a socket object
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(1)
+        print(f"Game server is on {self.host}:{self.port}")
+        self.main_server_socket, addr = self.server_socket.accept()
+        print("Connected to main server")
+        self.main_server_socket.send("connected".encode())
+        data = self.main_server_socket.recv(1024).decode()
+        if data == "start":
+            self.start_game()
 
-# Get local machine name
-host = socket.gethostname()
+    def start_game(self):
+        print(f"Waiting for 2 self.clients on {self.host}:{self.port}")
+        self.server_socket.listen(2)
+        # Accept connections from two self.clients
+        while True:
+            # Accept a connection
+            client_socket, addr = self.server_socket.accept()
+            client_thread = threading.Thread(target=self.handle_client, args=(client_socket, addr, self.port,))
+            client_thread.start()
 
-# Reserve a port for your service
-port = 12345
+    def handle_client(self, client_socket, addr, port):
+        print(f"Accepted connection from {addr} in port {port}")
+        self.clients.append(client_socket)
+        print(f"There are {len(self.clients)} self.clients")
+        # Receive the team data from the client
+        team_data = self.recvall(client_socket)
+        team = pickle.loads(team_data)
+        self.pokemon_teams.append(team)
+        if len(self.clients) == 2:
+            for c in self.clients:
+                c.send("start".encode())
+            print(f"Game starts on port {port}")
+            self.start_battle(self.clients, self.pokemon_teams)
+            print("Game ended")
 
-# Bind to the port
-server_socket.bind((host, port))
+    def start_battle(self, clients, pokemon_teams):
+        # Create instances of the active Pokémon for each team
+        team1 = self.pokemon_teams[0]
+        team2 = self.pokemon_teams[1]
+        active_pokemon1 = team1[0]
+        active_pokemon2 = team2[0]
 
-# Start listening for connections
-server_socket.listen(2)
-print("server is on")
+        self.send_pokemon_data(self.clients[0], active_pokemon1)
+        self.send_pokemon_data(self.clients[0], active_pokemon2)
+        self.send_pokemon_data(self.clients[1], active_pokemon2)
+        self.send_pokemon_data(self.clients[1], active_pokemon1)
+        # Conduct the battle
+        while True:
+            try:
+                # Get the moves from the self.clients
+                print("Waiting for both self.clients to send moves")
+                action1 = self.receive_move(self.clients[0])
+                action2 = self.receive_move(self.clients[1])
+                result = ""
+                print(action1, action2)
+                if "move" in action1 and "move" in action2:
+                    move1 = action1.split(':')[1]
+                    move2 = action2.split(':')[1]
 
-# Initialize a list to hold the client connections
-clients = []
-pokemon_teams = []
+                    # Determine the order of moves
+                    first_pokemon, second_pokemon = pokemon.determine_first_move(active_pokemon1, move1, active_pokemon2, move2)
+                    # Apply the moves
+                    if first_pokemon is active_pokemon1:
+                        result += active_pokemon2.damage_calculation(move1, active_pokemon1)
+                        time.sleep(0.001)
+                        result += active_pokemon1.damage_calculation(move2, active_pokemon2)
+                    else:
+                        result += active_pokemon1.damage_calculation(move2, active_pokemon2)
+                        time.sleep(0.001)
+                        result += active_pokemon2.damage_calculation(move1, active_pokemon1)
 
+                elif "switch" in action1 and "move" in action2:
+                    switching = action1.split(":")[1]
+                    for t in team1:
+                        if t.Name == switching:
+                            result += f"{active_pokemon1.Name} was switched to {switching}\n"
+                            active_pokemon1 = t
 
-# In your handle_client function
-def handle_client(client_socket,addr):
-    print(f"Accepted connection from {addr}")
-    clients.append(client_socket)
-    print(f"there are {len(clients)} clients")
-    # Receive the team data from the client
-    team_data = recvall(client_socket)
-    team = pickle.loads(team_data)
-    pokemon_teams.append(team)
-    for t in team: print(t)
-    if len(clients)==2:
-        for c in clients: c.send("start".encode())
-        start_battle()
+                    move = action2.split(":")[1]
+                    result += active_pokemon1.damage_calculation(move, active_pokemon2)
+                elif "switch" in action2 and "move" in action1:
+                    switching = action2.split(":")[1]
+                    for t in team2:
+                        if t.Name == switching:
+                            result += f"{active_pokemon2.Name} was switched to {switching}\n"
+                            active_pokemon2 = t
 
-
-
-def start_battle():
-    # Create instances of the active Pokémon for each team
-    team1 = pokemon_teams[0]
-    team2 = pokemon_teams[1]
-    active_pokemon1 = team1[0]
-    active_pokemon2 = team2[0]
-
-    send_pokemon_data(clients[0], active_pokemon1)
-    send_pokemon_data(clients[0],active_pokemon2)
-    send_pokemon_data(clients[1], active_pokemon2)
-    send_pokemon_data(clients[1],active_pokemon1)
-    # Conduct the battle
-    while True:
-        try:
-            # Get the moves from the clients
-            print("waiting for both clients to send moves")
-            action1 = receive_move(clients[0])
-            action2 = receive_move(clients[1])
-            result=""
-            print(action1,action2)
-            if "move" in action1 and "move" in action2:
-                move1=action1.split(':')[1]
-                move2=action2.split(':')[1]
-
-                # Determine the order of moves
-                first_pokemon, second_pokemon = pokemon.determine_first_move(active_pokemon1, move1, active_pokemon2, move2)
-                # Apply the moves
-                if first_pokemon is active_pokemon1:
-                    result+=active_pokemon2.damage_calculation(move1, active_pokemon1)
-                    time.sleep(0.001)
-                    result+=active_pokemon1.damage_calculation(move2, active_pokemon2)
+                    move = action1.split(":")[1]
+                    result += active_pokemon2.damage_calculation(move, active_pokemon1)
                 else:
-                    result+=active_pokemon1.damage_calculation(move2, active_pokemon2)
-                    time.sleep(0.001)
-                    result+=active_pokemon2.damage_calculation(move1, active_pokemon1)
-            
-            elif "switch" in action1 and "move" in action2:
-                switching=action1.split(":")[1]
-                for t in team1:
-                    if t.Name==switching:
-                        result+=f"{active_pokemon1.Name} was switched to {switching}\n"
-                        active_pokemon1=t
-                
-                move=action2.split(":")[1]
-                result+=active_pokemon1.damage_calculation(move, active_pokemon2)
-            elif "switch" in action2 and "move" in action1:
-                switching=action2.split(":")[1]
-                for t in team2:
-                    if t.Name==switching:
-                        result+=f"{active_pokemon2.Name} was switched to {switching}\n"
-                        active_pokemon2=t
+                    switching1 = action1.split(":")[1]
+                    switching2 = action2.split(":")[1]
+                    for t in team2:
+                        if t.Name == switching2:
+                            result += f"{active_pokemon2.Name} was switched to {switching2}\n"
+                            active_pokemon2 = t
 
-                move=action1.split(":")[1]
-                result+=active_pokemon2.damage_calculation(move, active_pokemon1)
-            else:
-                switching1=action1.split(":")[1]
-                switching2=action2.split(":")[1]
-                for t in team2:
-                    if t.Name==switching2:
-                        result+=f"{active_pokemon2.Name} was switched to {switching2}\n"
-                        active_pokemon2=t
-                    
-                for t in team1:
-                    if t.Name==switching1:
-                        result+=f"{active_pokemon2.Name} was switched to {switching1}\n"
-                        active_pokemon1=t
+                    for t in team1:
+                        if t.Name == switching1:
+                            result += f"{active_pokemon2.Name} was switched to {switching1}\n"
+                            active_pokemon1 = t
 
-            
-            print(result)
-            clients[0].send(result.encode())
-            clients[1].send(result.encode())
+                print(result)
+                self.send_result(self.clients[0], result)
+                self.send_result(self.clients[1], result)
 
-            
-            # Wait for both clients to request the result
-            responses = [False, False]
-            while not all(responses):
-                for i, client in enumerate(clients):
-                    response = client.recv(1024).decode()
-                    if response == "Give results":
-                        responses[i] = True
-            print("Sending Pokemon\n")
+                # Wait for both self.clients to request the result
+                responses = [False, False]
+                while not all(responses):
+                    for i, client in enumerate(self.clients):
+                        response = client.recv(1024).decode()
+                        if response == "Give results":
+                            responses[i] = True
+                print("Sending Pokemon\n")
 
-            # Send Pokémon status after both clients request it
-            send_pokemon_data(clients[0], active_pokemon1)
-            send_pokemon_data(clients[0], active_pokemon2)
-            send_pokemon_data(clients[1], active_pokemon2)
-            send_pokemon_data(clients[1], active_pokemon1)
+                # Send Pokémon status after both self.clients request it
+                self.send_pokemon_data(self.clients[0], active_pokemon1)
+                self.send_pokemon_data(self.clients[0], active_pokemon2)
+                self.send_pokemon_data(self.clients[1], active_pokemon2)
+                self.send_pokemon_data(self.clients[1], active_pokemon1)
 
-            team_data = pickle.dumps(team1)
-            data_len = len(team_data).to_bytes(4, byteorder='big')
-            clients[0].sendall(data_len + team_data)
+                team_data = pickle.dumps(team1)
+                data_len = len(team_data).to_bytes(4, byteorder='big')
+                self.clients[0].sendall(data_len + team_data)
 
-            team_data = pickle.dumps(team2)
-            data_len = len(team_data).to_bytes(4, byteorder='big')
-            clients[1].sendall(data_len + team_data)
+                team_data = pickle.dumps(team2)
+                data_len = len(team_data).to_bytes(4, byteorder='big')
+                self.clients[1].sendall(data_len + team_data)
 
-            
-            #sending results if someone won, or not
-            if not check_team(team1) or not check_team(team2):
-                if not check_team(team1):
-                    send_result(clients[0],"You Lost")
-                    send_result(clients[1],"You Won")
+                # Sending results if someone won, or not
+                if not self.check_team(team1) or not self.check_team(team2):
+                    if not self.check_team(team1):
+                        self.send_result(self.clients[0], "You Lost")
+                        self.send_result(self.clients[1], "You Won")
+                        break
+                    else:
+                        self.send_result(self.clients[1], "You Lost")
+                        self.send_result(self.clients[0], "You Won")
+                        break
                 else:
-                    send_result(clients[1],"You Lost")
-                    send_result(clients[0],"You Won")
-            else:
-                send_result(clients[0],"Next Turn")
-                send_result(clients[1],"Next Turn")
+                    self.send_result(self.clients[0], "Next Turn")
+                    self.send_result(self.clients[1], "Next Turn")
 
-        except (ConnectionResetError, BrokenPipeError):
-            # Handle client disconnection
-            if clients[0] in clients:
-                clients.remove(clients[0])
-                print("Client 1 disconnected. Client 2 wins.")
-                result = "Client 2 wins!"
-                if clients:
-                    for client in clients:
-                        try:
-                            send_result(client, result)
-                        except ConnectionResetError:
-                            pass
-            else:
-                clients.remove(clients[1])
-                print("Client 2 disconnected. Client 1 wins.")
-                result = "Client 1 wins!"
-                if clients:
-                    for client in clients:
-                        try:
-                            send_result(client, result)
-                        except ConnectionResetError:
-                            pass
-            break
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print(len(self.clients))
+                try:
+                    self.send_result(self.clients[0], "You Won")
+                    self.send_result(self.clients[1], "You Won")
+                    print(f"Sent result to {self.clients[0]}")
+                    break
+                except:
+                    pass
+                break
 
+    def recvall(self, sock):
+        # First, receive the message length (assume it's a 4-byte integer)
+        msg_len_data = b''
+        while len(msg_len_data) < 4:
+            chunk = sock.recv(4 - len(msg_len_data))
+            if not chunk:
+                raise RuntimeError("Socket connection broken")
+            msg_len_data += chunk
 
-def recvall(sock):
-    # First, receive the message length (assume it's a 4-byte integer)
-    msg_len_data = b''
-    while len(msg_len_data) < 4:
-        chunk = sock.recv(4 - len(msg_len_data))
-        if not chunk:
-            raise RuntimeError("Socket connection broken")
-        msg_len_data += chunk
+        msg_len = int.from_bytes(msg_len_data, byteorder='big')
 
-    msg_len = int.from_bytes(msg_len_data, byteorder='big')
+        # Now, receive the actual message data
+        data = b''
+        while len(data) < msg_len:
+            chunk = sock.recv(msg_len - len(data))
+            if not chunk:
+                raise RuntimeError("Socket connection broken")
+            data += chunk
 
-    # Now, receive the actual message data
-    data = b''
-    while len(data) < msg_len:
-        chunk = sock.recv(msg_len - len(data))
-        if not chunk:
-            raise RuntimeError("Socket connection broken")
-        data += chunk
+        return data
 
-    return data
+    def receive_move(self, client):
+        # Receive the move choice from the client
+        move_data = client.recv(1024)
+        move = move_data.decode()
+        return move
 
-def receive_move(client):
-    # Receive the move choice from the client
-    move_data = client.recv(1024)
-    move = move_data.decode()
-    return move
+    def send_pokemon_data(self, client, pokemon):
+        # Serialize the Pokémon object
+        pokemon_data = pickle.dumps(pokemon)
 
-def send_pokemon_data(client, pokemon):
-    # Serialize the Pokémon object
-    pokemon_data = pickle.dumps(pokemon)
+        data_len = len(pokemon_data).to_bytes(4, byteorder='big')
+        client.sendall(data_len + pokemon_data)
 
-    data_len = len(pokemon_data).to_bytes(4, byteorder='big')
-    client.sendall(data_len + pokemon_data)
+    def send_result(self, client, result):
+        # Send the result to the client
+        client.send(result.encode())
 
-def send_result(client, result):
-    # Send the result to the client
-    client.send(result.encode())
-
-def check_team(team):
-    #return false if no pokemon has hp in the team
-    for t in team:
-        if t.HP>0:
-            return True
-    return False
-
-while True:
-    # Accept a connection
-    client_socket, addr = server_socket.accept()
-    client_thread = threading.Thread(target=handle_client, args=(client_socket,addr,))
-    client_thread.start()
-
+    def check_team(self, team):
+        # Return false if no pokemon has hp in the team
+        for t in team:
+            if t.HP > 0:
+                return True
+        return False
---
+++ test_server.py
@@ -0,0 +1,283 @@
+import socket
+import threading
+import pickle
+from Pokemon import pokemon
+import random
+import time
+import server_battle
+from server_battle import Game_Server
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+import sqlite3
+
+
+# Generate a private key for the server
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+
+# Generate a public key for the server
+public_key = private_key.public_key()
+
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Get local machine name
+host = socket.gethostname()
+
+# Reserve a port for your service
+port = 6969
+
+# Bind to the port
+server_socket.bind((host, port))
+
+# Start listening for connections
+server_socket.listen()
+print("server is on")
+
+# Initialize a list to hold the client connections
+clients = []
+server_ports = []
+
+# In your handle_client function
+def handle_client(client_socket,addr):
+    print(f"Accepted connection from {addr}")
+    clients.append(client_socket)
+    print(f"there are {len(clients)} clients")
+
+    while True:
+        data=client_socket.recv(1024).decode()
+        print(data)
+        if "give public key" in data:
+            send_public_key(client_socket)
+
+        elif "sign up" in data:
+            encrypted_username = recvall(client_socket)
+            decrypted_username = decrypt_data(encrypted_username)
+            username = decrypted_username
+
+            encrypted_password = recvall(client_socket)
+
+            if check_sign_in(username):
+                client_socket.send("failure".encode())
+            else:
+                add_user_to_database(username, encrypted_password)
+                client_socket.send("success".encode()) 
+
+        elif "log in" in data:
+            encrypted_username = recvall(client_socket)
+            decrypted_username = decrypt_data(encrypted_username)
+            username = decrypted_username
+
+            encrypted_password = recvall(client_socket)
+
+            if check_login(username, decrypt_data(encrypted_password)):
+                client_socket.send("success".encode())
+                data=client_socket.recv(1024).decode()
+                if "team" in data:
+                    team = get_team_data(username)
+                    if team!=None:
+                        client_socket.send("yes".encode())
+                        time.sleep(0.01)
+                        team_data = pickle.dumps(team)
+                        team_data_len = len(team_data).to_bytes(4, byteorder='big')
+                        client_socket.sendall(team_data_len + team_data)
+            else:
+                client_socket.send("failure".encode())
+
+            
+
+        elif "edit team" in data:
+            team_data = recvall(client_socket)
+            username = data.split(":")[-1]
+            print(f"team data received from {username}")
+            save_team_data(team_data,username)
+            get_team_data(username)
+                
+        elif "create room" in data:
+            generated_port = random.choice([p for p in range(12345, 23456) if p not in server_ports])
+            username = data.split(":")[-1]
+            if get_team_data(username)!=None:
+                client_socket.send("ok".encode())
+                data=client_socket.recv(1024).decode()
+                if data=="give code":
+                    client_socket.send(f"{generated_port}".encode())
+                    server_ports.append(generated_port)
+                    game_server = Game_Server(host, generated_port)
+                    game_thread = threading.Thread(target=game_server.start_server)
+                    game_thread.start()
+
+                    #connect to the game server
+                    game_client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    print(f"connecting to {host}:{generated_port}")
+                    game_client.connect((host,generated_port))
+                    data=game_client.recv(1024).decode()
+                    if data=="connected":
+                        print(f"connected to {generated_port}")
+                        game_client.send("start".encode())
+            else:
+                client_socket.send("no team".encode())
+
+        elif "join room" in data:
+            username = data.split(":")[-1]
+            if get_team_data(username)!=None:
+                print("team found")
+                client_socket.send("ok".encode())
+                data=client_socket.recv(1024).decode()
+                if "get rooms" in data:
+                    print("sending rooms")
+                    client_socket.send(",".join(str(p) for p in server_ports).encode())
+                    data=client_socket.recv(1024).decode()
+                    if "connect me to" in data:
+                        print(f"connected to {data.split()[-1]}")
+                        client_socket.send("connected".encode())
+                        time.sleep(0.01)
+                        print("sending team")
+                        team = get_team_data(username)
+                        team_data = pickle.dumps(team)
+                        team_data_len = len(team_data).to_bytes(4, byteorder='big')
+                        client_socket.sendall(team_data_len + team_data)
+            else:
+                client_socket.send("no team".encode())
+
+        elif "exit" in data:
+            print(f"closing connection with {addr}")
+            username = data.split(":")[-1]
+            print(f"{username} is exiting")
+            conn = sqlite3.connect('users.db')
+            cursor = conn.cursor()
+            cursor.execute('UPDATE users SET status="offline" WHERE username=?', (username,))
+            conn.commit()
+            conn.close()
+            client_socket.close()
+            clients.remove(client_socket)
+            break
+                
+
+def recvall(sock):
+    # First, receive the message length (assume it's a 4-byte integer)
+    msg_len_data = b''
+    while len(msg_len_data) < 4:
+        chunk = sock.recv(4 - len(msg_len_data))
+        if not chunk:
+            raise RuntimeError("Socket connection broken")
+        msg_len_data += chunk
+
+    msg_len = int.from_bytes(msg_len_data, byteorder='big')
+
+    # Now, receive the actual message data
+    data = b''
+    while len(data) < msg_len:
+        chunk = sock.recv(msg_len - len(data))
+        if not chunk:
+            raise RuntimeError("Socket connection broken")
+        data += chunk
+
+    return data
+
+def setup_database():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    DROP TABLE IF EXISTS users
+                   ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        current_team TEXT,
+        status TEXT CHECK(status IN ('online', 'offline')) NOT NULL DEFAULT 'offline',
+        last_fought_against TEXT
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
+def check_sign_in(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username=?', (username,))
+    user = cursor.fetchone()
+    if user:  return True
+    conn.close()
+    return False
+
+def check_login(username, password):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username=?', (username,))
+    user = cursor.fetchone()
+    if user: 
+        status=user[4]
+        decrypted_password=decrypt_data(user[2])
+        print(decrypted_password, status)
+        if status=="offline" and password==decrypted_password:
+            cursor.execute('UPDATE users SET status="online" WHERE username=?', (username,))
+            conn.commit()
+            return True
+    conn.close()
+    return False
+
+def add_user_to_database(username, encrypted_password_hex):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    # Insert the username and encrypted password into the database
+    cursor.execute('INSERT INTO users (username, password, status) VALUES (?, ?, "online")', (username, encrypted_password_hex))
+    conn.commit()
+    conn.close()
+
+
+def send_public_key(client_socket):
+    public_key_data = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    public_key_data = pickle.dumps(public_key_data)
+    public_key_data_len = len(public_key_data).to_bytes(4, byteorder='big')
+    client_socket.sendall(public_key_data_len + public_key_data)
+
+def decrypt_data(encrypted_password):
+    decrypted_password = private_key.decrypt(
+        encrypted_password,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return decrypted_password.decode()
+
+def save_team_data(team,username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE users SET current_team=? WHERE username=?', (team, username))
+    conn.commit()
+    conn.close()
+
+def get_team_data(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT current_team FROM users WHERE username=?', (username,))
+    team_data = cursor.fetchone()
+    conn.close()
+    if team_data[0]==None: 
+        print("No team data found")
+        return None
+    team=pickle.loads(team_data[0])
+    return team
+
+
+
+setup_database()
+while True:
+    # Accept a connection
+    client_socket, addr = server_socket.accept()
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,addr,))
+    client_thread.start()
